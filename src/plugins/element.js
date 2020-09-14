67205384a21a36109e2385f035af1c14f830b017
liujianping <754844887@qq.com>
2020-09-14 11:22:17 +0800

    init commit

---
+++ src/plugins/element.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import { 
+    Button, Container, Aside, Header,
+    Main, Menu, Submenu, MenuItem,
+    MenuItemGroup, Dropdown, DropdownItem,
+    DropdownMenu, Table, TableColumn
+ } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Dropdown)
+Vue.use(DropdownItem)
+Vue.use(DropdownMenu)
+Vue.use(Table)
+Vue.use(TableColumn)
